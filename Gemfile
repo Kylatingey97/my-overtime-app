d6efa30f2569561df8d3ab343199210a556f91af
Kyla  Tingey <kylatingey97@gmail.com>
2017-02-12 12:57:44 -0700

    Implemented Administrate admin dashboard

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ git_source(:github) do |repo_name|
 end
 
 
-gem 'rails', '~> 5.0.1'
+gem 'rails'
 gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.0'
 gem 'sass-rails', '~> 5.0'
@@ -36,3 +36,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise', '~> 4.2'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
 gem "gritter", "1.2.0"
+gem 'administrate' 
+gem 'bourbon'
--- db/schema.rb
+++ db/schema.rb
@@ -21,9 +21,8 @@ ActiveRecord::Schema.define(version: 20170211201224) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
-
-  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
