83095903864e2215509c83979fe9c69f6eab2206
Kyla  Tingey <kylatingey97@gmail.com>
2017-02-11 19:18:44 -0700

    Implmented index view with posts

--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,9 @@ ActiveRecord::Schema.define(version: 20170211201224) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
