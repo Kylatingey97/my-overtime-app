7597a8313f0950f85e988d2c4a7ca5c85cc53b51
Kyla  Tingey <kylatingey97@gmail.com>
2017-03-02 09:35:16 -0700

    Implemented functional buttons for employee homepage confirmations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221165021) do
+ActiveRecord::Schema.define(version: 20170302024104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
