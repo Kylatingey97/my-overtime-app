0879f840ae26f953f88299e9cc5312ca1b039a7a
Kyla  Tingey <kylatingey97@gmail.com>
2017-03-03 09:36:46 -0700

    Made changes in files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,8 +16,3 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'my-overtime-application.herokuapp.com' }
 end
 
-if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
-  end
